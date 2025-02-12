298680c57e9ad8af9644d7aac1c791207f1749c6
Vinícius Massari <stonefields11@hotmail.com>
2025-02-12 18:04:32 -0300

    :sparkles: feat: Home Page countries card info logic added

--- src/app/components/country-card/country-card.component.ts
+++ src/app/components/country-card/country-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, InputSignal } from '@angular/core';
 import { CountryCardDetails } from '../../../types/countryCardDetails';
 import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-country-card',
-  imports: [RouterLink],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './country-card.component.html',
 })
 export class CountryCardComponent {
