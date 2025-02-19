66aff6e626342ddf40d740b51782cb71ed18faa7
Vinícius Massari <stonefields11@hotmail.com>
2025-02-19 14:41:26 -0300

    :white_check_mark: test: cypress unit tests refactored

--- cypress/components/country-card.cy.ts
+++ cypress/components/country-card.cy.ts
@@ -1,7 +1,18 @@
+import { of } from 'rxjs';
 import { CountryCardComponent } from '../../src/app/components/country-card/country-card.component';
+import { ActivatedRoute } from '@angular/router';
 it('Should mount the component', () => {
   cy.fixture('countryData.json').then((countryData) => {
     cy.mount(CountryCardComponent, {
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of({ get: () => 'South Georgia' }), // simulate route param
+            snapshot: { paramMap: { get: () => 'South Georgia' } }, // simulate  this.activatedRoute.params.subscribe() call
+          },
+        },
+      ],
       componentProperties: {
         countryCardInfo: countryData,
       },
